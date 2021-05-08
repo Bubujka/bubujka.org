4bc8e7e0ad041b0ed12cd9821850ec15b1cf7d26
Aleksej Kamynin <zendzirou@gmail.com>
2021-05-08 15:21:42 +0300

    Работа над обложками

--- make-cover.js
+++ make-cover.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 const puppeteer = require('puppeteer');
+const glob = require('glob');
+const path = require('path');
+
+
+var d = glob.sync('_cover/*.md');
+
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -8,9 +14,13 @@ const puppeteer = require('puppeteer');
       width: 1920,
       height: 1080
   })
-  await page.goto('http://localhost:3000/cover/1.html', { waitUntil: 'networkidle0', });
+  for(var pth of d){
+    var code = path.basename(pth, '.md');
+    console.log('Обрабатываю: '+code);
 
-  await page.screenshot({ path: 'cover1.png' });
+    await page.goto('http://localhost:3000/cover/'+code+'.html', { waitUntil: 'networkidle0', });
+    await page.screenshot({ path: 'covers/'+code+'.png' });
+  }
 
   await browser.close();
 })();
